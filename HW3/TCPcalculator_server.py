c7ec8b38d9d9bee2b538b4f53437b93151107342
Sieun Lee <melonisha12@naver.com>
2022-03-28 18:58:38 +0900

    TCPcalculator_server.py

--- HW3/TCPcalculator_server.py
+++ HW3/TCPcalculator_server.py
@@ -8,30 +8,29 @@ print('waiting...')
 while True:
     client, addr = s.accept()
     print('connection from ', addr)
-    
-    a = (client.recv(1024).decode())
-    b = a.split()
-    first = b[0]
-    first = int(first) 
-    op = b[1]
-    second = b[2]
-    second = int(second)
+    while True:
+        a = (client.recv(1024).decode())
+        if a in 'q':
+            client.close()
+            break
+        
+        b = a.split()        
+        first = b[0]
+        first = int(first) 
+        op = b[1]
+        second = b[2]
+        second = int(second)
 
-    if op == '+':
-        rsp = first + second
-        rsp = str(rsp)
-        client.send(rsp.encode())
-    elif op == '-':
-        rsp = first - second
-        rsp = str(rsp)
-        client.send(rsp.encode())
-    elif op == '*':
-        rsp = first * second
-        rsp = str(rsp)
-        client.send(rsp.encode())
-    elif op == '/':
-        rsp = first / second
-        rsp = round(rsp, 1)
+        if op == "+":
+            rsp = first + second
+        elif op == "-":
+            rsp = first - second
+        elif op == "*":
+            rsp = first * second
+        elif op == "/":
+            rsp = first / second
+            rsp = round(rsp, 1)
+
         rsp = str(rsp)
         client.send(rsp.encode())
 
